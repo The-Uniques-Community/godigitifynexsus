ea924b100d11301bc322a1d1d181de563e69ae1e
SuperiorSU <sujalsinha2001@gmail.com>
2025-07-22 13:39:32 +0530

    all set

---
+++ src/pages/home/components/DigitalStrategy.jsx
@@ -0,0 +1,11 @@
+
+const Card = ({num, heading, description, bgColor})=>{
+    
+}
+const DigitalStrategy = () => {
+  return (
+    <div>DigitalStrategy</div>
+  )
+}
+
+export default DigitalStrategy
