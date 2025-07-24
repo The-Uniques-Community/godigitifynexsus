1e618710eb6018e5216047f3b7f7d83f3db048b2
Your Name <abhimishra.db12@gmail.com>
2025-07-24 22:56:37 +0530

    Update Project component - remove heading, make title purple, and update index

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Link } from 'react-router'
 import Hero from './components/Hero'
 import About from './components/About'
+import Project from './Project'
 
 const index = () => {
   return (
     <div>
         <Hero />
-        <About />x``
+        <About />
+        <Project />
     </div>
   )
 }
