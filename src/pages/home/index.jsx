5fa2ffce04cdcfa5f8b570a86240f80a21eeab3c
Your Name <abhimishra.db12@gmail.com>
2025-07-25 10:59:57 +0530

    tt

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,21 +5,17 @@ import Blog from "./components/Blog";
 import Service from "./components/Service";
 import CompanyLogos from "./components/CompanyLogos";
 import DigitalStrategy from "./components/DigitalStrategy";
-import React from 'react'
-import { Link } from 'react-router'
-import Hero from './components/Hero'
-import About from './components/About'
+
 import Project from './Project'
 
 const index = () => {
   return (
     <div>
-        <Hero />
-        <About />
-        <Project />
+   
       <Hero />
       <About />
       <DigitalStrategy/>
+      <Project />
       <Blog />
       <Service />
       <CompanyLogos/>
